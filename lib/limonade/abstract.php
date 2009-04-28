c4bec9c611eb870e0637054b230fba3fc948aff0
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-04-28 11:33:18 +0200

    Adding abstract functions definitions for documentation

---
+++ lib/limonade/abstract.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Abstract methods that might be redefined by user
+ * Do not include this file in your app: it only aims to provide documentation
+ * about those functions.
+ * 
+ * @package limonade
+ * @subpackage abstract
+ */
+ 
+/**
+ * It will be called when app is launched (at the begining og the run function).
+ * You can define options inside it, a connection to a database ...
+ *
+ * @return void 
+ */
+function configure()
+{
+  return;
+}
+ 
+/**
+ * Called before each request.
+ * This is very useful to define a default layout or passing common variables
+ * to the templates
+ *
+ * @return void 
+ */
+function before()
+{
+  
+}
+ 
+/**
+ * An `after` output filter
+ * 
+ * Called after each request and can apply a transformation to the output
+ * (except for `render_file` outputs  which are sent directly to the output buffer)
+ *
+ * @param string $output 
+ * @return string 
+ */
+function after($output)
+{
+  # Call functions...
+  # .. modifies $output...
+  return $output;
+}
+ 
+/**
+ * Not found error output
+ *
+ * @param string $errno 
+ * @param string $errstr 
+ * @param string $errfile 
+ * @param string $errline 
+ * @return string "not found" output string
+ */
+function not_found($errno, $errstr, $errfile=null, $errline=null)
+{
+ 
+}
+ 
+/**
+ * Server error output
+ *
+ * @param string $errno 
+ * @param string $errstr 
+ * @param string $errfile 
+ * @param string $errline 
+ * @return string "server error" output string
+ */
+function server_error($errno, $errstr, $errfile=null, $errline=null)
+{
+  
+}
+ 
+/**
+ * Called when a route is not found.
+ *
+ * @param string $request_method 
+ * @param string $request_uri 
+ * @return void 
+ */
+function route_missing($request_method, $request_uri)
+{
+  halt(NOT_FOUND, "($request_method) $request_uri"); # by default
+}
+ 
+?>
