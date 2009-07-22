47652893d012b51eb6a21da570ebddd4b4c874ad
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-07-22 17:55:28 +0200

    Revert "Movie internal routes at the begining of the lib in order to be sure that they will not be by passed by a user 'everythin else' route ('/**'...)."
    This reverts commit debd590a00cf0a2c97e112866df3a7cc3c767477.

--- lib/limonade.php
+++ lib/limonade.php
@@ -127,35 +127,6 @@ if(get_magic_quotes_runtime()) set_magic_quotes_runtime(false);
 ini_set('display_errors', 0);
 
 
-## SETTING INTERNAL ROUTES _____________________________________________________
-
-dispatch(array("/_lim_css/*.css", array('_lim_css_filename')), 'render_limonade_css');
-  /**
-   * Internal controller that responds to route /_lim_css/*.css
-   *
-   * @access private
-   * @return string
-   */
-  function render_limonade_css()
-  {
-    option('views_dir', file_path(option('limonade_public_dir'), 'css'));
-    $fpath = file_path(params('_lim_css_filename').".css");
-    return css($fpath, null);
-  }
-
-dispatch(array("/_lim_public/**", array('_lim_public_file')), 'render_limonade_file');
-  /**
-   * Internal controller that responds to route /_lim_public/**
-   *
-   * @access private
-   * @return void
-   */
-  function render_limonade_file()
-  {
-    $fpath = file_path(option('limonade_public_dir'), params('_lim_public_file'));
-    return render_file($fpath, true);
-  }
-
 
 
 
@@ -357,8 +328,35 @@ function run($env = null)
     }
   }  
   
+  # 6. Set default routes used for default views
+  dispatch(array("/_lim_css/*.css", array('_lim_css_filename')), 'render_limonade_css');
+    /**
+     * Internal controller that responds to route /_lim_css/*.css
+     *
+     * @access private
+     * @return string
+     */
+    function render_limonade_css()
+    {
+      option('views_dir', file_path(option('limonade_public_dir'), 'css'));
+      $fpath = file_path(params('_lim_css_filename').".css");
+      return css($fpath);
+    }
   
-  # 6. Check request
+  dispatch(array("/_lim_public/**", array('_lim_public_file')), 'render_limonade_file');
+    /**
+     * Internal controller that responds to route /_lim_public/**
+     *
+     * @access private
+     * @return void
+     */
+    function render_limonade_file()
+    {
+      $fpath = file_path(option('limonade_public_dir'), params('_lim_public_file'));
+      return render_file($fpath, true);
+    }
+  
+  # 7. Check request
   if($rm = request_method())
   {
     if(!request_method_is_allowed($rm))
