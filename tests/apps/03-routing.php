fc37e1a5504cee97784d901978b8e52adc38cdec
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-11-21 19:48:17 +0100

    Add functional tests: testing dispatch_*

---
+++ tests/apps/03-routing.php
@@ -0,0 +1,140 @@
+<?php
+
+require_once dirname(dirname(dirname(__FILE__))).'/lib/limonade.php';
+
+/* testing with all callbacks cases */
+dispatch('/route0', 'test_route0');
+function test_route0()
+{
+  return "route0";
+}
+
+dispatch('/route1', 'test_route1');
+function test_route1()
+{
+  return "route1";
+}
+
+
+class MyController
+{
+  var $num = 10;
+  
+  function __construct($num = 10)
+  {
+    $this->num = $num;
+  }
+  
+  public function method($value=1)
+  {
+    return $value * $this->num;
+  }
+ 
+  static public function staticMethod($value=1)
+  {
+   return $value * 20;
+  }
+}
+
+
+$obj = new MyController(10);
+
+dispatch('/route2', array($obj, 'method'));
+dispatch('/route3', array('MyController', 'staticMethod'));
+dispatch('/route4', 'MyController::staticMethod');
+
+/* parameterized routes */
+dispatch('/route5', 'test_route5', array('params' => array('type' => 'human')));
+function test_route5()
+{
+  return params('type');
+}
+dispatch('/route5b', 'test_route5b', array('params' => array('human')));
+function test_route5b()
+{
+  return params(0);
+}
+dispatch('/route6', 'test_route6', array('params' => array('type' => 'human')));
+function test_route6($type)
+{
+  return $type;
+}
+dispatch('/route6b', 'test_route6b', array('params' => array('type' => 'human', 'num' => 10)));
+dispatch('/route6c', 'test_route6b', array('params' => array('human', 10)));
+function test_route6b($type, $num)
+{
+  return $type . $num;
+}
+
+
+/* routes params */
+dispatch('/route7/:id', 'test_route7');
+function test_route7()
+{
+  $res = params('id');
+  (int) $res;
+  return $res;
+}
+dispatch('/route7b/:id', 'test_route7b', array('params' => array('id' => 10)));
+function test_route7b()
+{
+  $res = params('id');
+  (int) $res;
+  return $res;
+}
+dispatch('/route7c/:id', array($obj, 'method'));
+dispatch('/route7d/:id', array($obj, 'method'), array('params' => array('id' => 10)));
+dispatch('/route7e/:id', 'MyController::staticMethod');
+dispatch('/route7f/:id', 'MyController::staticMethod', array('params' => array('id' => 10)));
+dispatch('/route7g', 'MyController::staticMethod', array('params' => array('id' => 10)));
+dispatch('/route7h', 'MyController::staticMethod', array('params' => array(10)));
+dispatch('/route8/:id', 'test_route8');
+function test_route8($id)
+{
+  (int) $id;
+  return $id;
+}
+dispatch('/route8b/:id', 'test_route8b', array('params' => array('id' => 10)));
+function test_route8b($id)
+{
+  (int) $id;
+  return $id;
+}
+dispatch('/route9/*', 'MyController::staticMethod');
+dispatch('/route9b/*', 'MyController::staticMethod', array('params' => array(10)));
+dispatch(array('/route10/*', array('id')), 'MyController::staticMethod');
+dispatch(array('/route10b/*', array('id')), 'MyController::staticMethod', array('params' => array('id' => 10)));
+
+/* http methods dispatching */
+
+dispatch_get('/route11', 'test_route11');
+function test_route11()
+{
+  header('X-LIM-CTL: route11');
+  return "GET";
+}
+
+dispatch_post('/route11', 'test_route11post');
+function test_route11post()
+{
+  header('Content-length: 4');
+  return "POST";
+}
+
+dispatch_put('/route11', 'test_route11put');
+function test_route11put()
+{
+  return "PUT";
+}
+
+dispatch_delete('/route11', 'test_route11delete');
+function test_route11delete()
+{
+  return "DELETE";
+}
+
+
+
+
+
+run();
--- tests/functional.php
+++ tests/functional.php
@@ -14,6 +14,78 @@ test_case("Functional");
      $response =  test_request(TESTS_DOC_ROOT.'01-hello_world.php', 'GET', true);
      //echo $response;
      assert_header($response, 'Content-type', 'text/html');
+   }
+   
+   function test_functional_routing()
+   {
+     $path = TESTS_DOC_ROOT.'03-routing.php/';
+     $response =  test_request($path.'route0', 'GET');
+     assert_equal($response, 'route0');
+     $response =  test_request($path.'route1', 'GET');
+     assert_equal($response, 'route1');
+     $response =  test_request($path.'route2', 'GET');
+     assert_equal($response, 10);
+     $response =  test_request($path.'route3', 'GET');
+     assert_equal($response, 20);
+     $response =  test_request($path.'route4', 'GET');
+     assert_equal($response, 20);
+     
+     $response =  test_request($path.'route5', 'GET');
+     assert_equal($response, 'human');
+     $response =  test_request($path.'route5b', 'GET');
+      assert_equal($response, 'human');
+     $response =  test_request($path.'route6', 'GET');
+     assert_equal($response, 'human');
+     $response =  test_request($path.'route6b', 'GET');
+     assert_equal($response, 'human10');
+     $response =  test_request($path.'route6c', 'GET');
+     assert_equal($response, 'human10');
+     
+     $response =  test_request($path.'route7/123', 'GET');
+     assert_equal($response, 123);
+     $response =  test_request($path.'route7b/123', 'GET');
+     assert_equal($response, 123);
+     $response =  test_request($path.'route7c/123', 'GET');
+     assert_equal($response, 1230);
+     $response =  test_request($path.'route7d/123', 'GET');
+     assert_equal($response, 1230);
+     $response =  test_request($path.'route7e/123', 'GET');
+     assert_equal($response, 2460);
+     $response =  test_request($path.'route7f/123', 'GET');
+     assert_equal($response, 2460);
+     $response =  test_request($path.'route7g', 'GET');
+     assert_equal($response, 200);
+     $response =  test_request($path.'route7h', 'GET');
+     assert_equal($response, 200);
+     $response =  test_request($path.'route8/123', 'GET');
+     assert_equal($response, 123);
+     $response =  test_request($path.'route8b/123', 'GET');
+     assert_equal($response, 123);
+     $response =  test_request($path.'route9/123', 'GET');
+     assert_equal($response, 2460);
+     $response =  test_request($path.'route9b/123', 'GET');
+     assert_equal($response, 2460);
+     $response =  test_request($path.'route10/123', 'GET');
+     assert_equal($response, 2460);
+     $response =  test_request($path.'route10b/123', 'GET');
+     assert_equal($response, 2460);
+     
+     /* http methods dispatching */
+     $response =  test_request($path.'route11', 'GET');
+     assert_equal($response, 'GET');
+     $response =  test_request($path.'route11', 'POST');
+     var_dump($response);
+     assert_equal($response, 'POST');
+     $response =  test_request($path.'route11', 'PUT');
+     assert_equal($response, 'PUT');
+     $response =  test_request($path.'route11', 'DELETE');
+     assert_equal($response, 'DELETE');
+     $response =  test_request($path.'route11', 'HEAD', true);
+     assert_header($response, 'X-LIM-CTL', 'route11');
+     
+     /* undefined route */
+     $response =  test_request($path.'unknown_route', 'GET');
+     assert_match('/Page not found/', $response);     
      
    }
 end_test_case();
